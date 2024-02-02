03aef773552844a5de1f306376c83b87f44bc56c
mitarashi87 <153958511+mitarashi87@users.noreply.github.com>
2024-02-03 00:11:11 +0900

    コンフィグの実装

--- src/main/java/io/github/mitarashi87/othello/Config.java
+++ src/main/java/io/github/mitarashi87/othello/Config.java
@@ -3,7 +3,7 @@ package io.github.mitarashi87.othello;
 import java.util.ResourceBundle;
 
 public class Config {
-	public static final String fileName = "config";
+	public static final String fileName = "application";
 	public static final ResourceBundle bundle = ResourceBundle.getBundle(fileName);
 	public static final String stdInEncode = bundle.getString("stdInEncode");
 	public static final String host = bundle.getString("host");
