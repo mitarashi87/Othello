474a62440688c278522508a5cb1336d8abb92f38
mitarashi87 <153958511+mitarashi87@users.noreply.github.com>
2024-02-03 07:36:20 +0900

    サーバーコンソールコマンドの処理を別メソッドに移動

--- src/main/java/io/github/mitarashi87/othello/server/ServerConsole.java
+++ src/main/java/io/github/mitarashi87/othello/server/ServerConsole.java
@@ -1,8 +1,11 @@
 package io.github.mitarashi87.othello.server;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import io.github.mitarashi87.othello.player.CuiPlayer;
 import io.github.mitarashi87.othello.player.Player;
 
@@ -15,24 +18,20 @@ public class ServerConsole {
 
 	public List<Player> launchMatchingRoom() {
 		List<CuiPlayer> cuiPlayers = new ArrayList<>();
+		CommandActions actions = new CommandActions(cuiPlayers);
+
+		String helpCommand = "help";
+		Map<String, Consumer<String>> actionMap = this.getActionMap(helpCommand, actions);
 
 		// 標準入力
 		System.out.println();
 		System.out.println("サーバーコンソールを起動しました");
+		actionMap.get(helpCommand).accept("");
 
-		while (true) {
+		while (!actions.finished) {
 			System.out.print("> ");
-			String command = sc.nextLine();
-			if (command.equals("/help")) {
-				System.out.println("TODO_ここでヘルプを表示");
-			}
-			if (command.equals("/start")) {
-				System.out.println("プレイヤーの待ち受けを終了します。");
-				break;
-			}
-			if (command.equals("/invite")) {
-				cuiPlayers.add(CuiPlayer.create(sc));
-			}
+			String input = sc.nextLine();
+			this.tryParseAndExsecCommand(input, actionMap, helpCommand);
 		}
 
 		// 1人のCUIプレイヤー以外は全体メッセージを受け取っても表示しない
@@ -46,4 +45,75 @@ public class ServerConsole {
 		List<Player> players = List.copyOf(cuiPlayers);
 		return players;
 	}
+
+	private Map<String, Consumer<String>> getActionMap(String helpCommand, CommandActions actions) {
+
+		Map<String, Consumer<String>> actionMap = new HashMap<>();
+
+		actionMap.put(helpCommand, (args) -> {
+		});
+		actionMap.put("invite", actions::invite);
+		actionMap.put("start", actions::start);
+		actionMap.put(helpCommand, (args) -> {
+			System.out.println("使用可能コマンド一覧");
+			String commandlist = String.join(", ", actionMap.keySet());
+			System.out.println(commandlist);
+		});
+		return actionMap;
+	}
+
+	private Boolean tryParseAndExsecCommand(
+			String input,
+			Map<String, Consumer<String>> actionMap,
+			String helpCommand) {
+		String commandPrefix = "/";
+		String commandSeparator = " ";
+		Boolean isCommand = input.startsWith(commandPrefix);
+		if (isCommand) {
+			// "/command arg1 arg2" -> "command arg1 arg2"
+			String withoutPrefix = input.substring(commandPrefix.length());
+			// "command arg1 arg2" -> "command" "arg1" "arg2"
+			String[] commandAndArgs = withoutPrefix.split(commandSeparator);
+			// "command" "arg1" "arg2" -> "command"
+			String command = commandAndArgs[0];
+			if (actionMap.containsKey(command)) {
+				Consumer<String> action = actionMap.get(command);
+				// "command arg1 arg2" -> "command"+" " -> 8
+				Integer argsPartFirstIndex = command.length() + commandSeparator.length();
+				// "command arg1 arg2" -> "arg1 arg2"
+				String commandArgs =
+						withoutPrefix.length() < argsPartFirstIndex
+								? ""
+								: withoutPrefix.substring(argsPartFirstIndex);
+				// run action
+				action.accept(commandArgs);
+			} else {
+				System.out.println("該当するコマンドがありません。");
+				actionMap.get(helpCommand).accept("");
+			}
+		}
+		return isCommand;
+	}
+
+	/**
+	 * コマンド挙動実装群
+	 */
+	private class CommandActions {
+		private List<CuiPlayer> cuiPlayers;
+		private Boolean finished = false;
+
+		public CommandActions(List<CuiPlayer> cuiPlayers) {
+			this.cuiPlayers = cuiPlayers;
+		}
+
+		public void invite(String args) {
+			cuiPlayers.add(CuiPlayer.create(sc));
+		}
+
+		public void start(String args) {
+			System.out.println("プレイヤーの待ち受けを終了します。");
+			this.finished = true;
+		}
+	}
 }
+
